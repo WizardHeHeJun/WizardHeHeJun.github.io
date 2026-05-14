544b34b316c94d03cb93eba1535f1ad823e00aad
WizardHeHeJun <1617186107@qq.com>
2026-05-14 11:45:55 +0800

    feat: customize site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "WizardHeHeJun's Notes";
+export const SITE_DESCRIPTION = '项目分享、技术笔记，和一些不想被时间冲走的碎碎念。';
